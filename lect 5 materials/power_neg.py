141ac9918514e13daf61d0961d5793dc42d1b55c
ksetdekov <Qe82SPsYawm2vwFpHrCc>
2020-10-08 13:54:10 +0300

    dumb power_neg.py

--- lect 5 materials/power_neg.py
+++ lect 5 materials/power_neg.py
@@ -1,23 +1,22 @@
 a_val = float(input())
 b_val = int(input())
 
-
-def power_pos(a, n):
-    if n > 1:
-        return a * power(a, n - 1)
-    elif n == 1:
-        return a
-    else:
-        return 1
-
-
 def power(a, n):
-    if n > 0:
-        return a * power_pos(a, n - 1)
-    elif n == 0:
+    if n == 0:
         return 1
+    elif n > 0:
+        res = 1
+        while n > 0:
+            res *= a
+            n -= 1
+        return res
     elif n < 0:
-        return 1 / (a * power_pos(a, -n - 1))
+        n = -n
+        res = 1
+        while n > 0:
+            res *= a
+            n -= 1
+        return 1/res
 
 
 result = power(a_val, b_val)
