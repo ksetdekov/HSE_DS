f361fda9a864d1feb70e281feb092459acc9f6b8
Kirill Setdekov <ksetdekov@gmail.com>
2020-10-10 12:08:06 +0300

    solved v_lagrange.py

--- lect 5 materials/v lagrange.py
+++ lect 5 materials/v lagrange.py
@@ -1,7 +1,3 @@
-n_inp = 1000
-result = []
-
-
 def integer_sqrt(n):
     if n < 0:
         raise ValueError("works on non-negative")
@@ -16,43 +12,47 @@ def integer_sqrt(n):
             return large_candidate
 
 
+n_inp = int(input())
+result = []
+
 a_level = integer_sqrt(n_inp)
-for a in range(a_level, 0, -1):
+for a in range(a_level, -1, -1):
     b_inp = n_inp - a ** 2
     b_level = integer_sqrt(b_inp)
-    print(a, 'step a')
+    # print(a, 'step a')
     if b_inp == 0:
         result.append(a)
         break
     # loop 1
 
-    for b in range(b_level, 0, -1):
+    for b in range(b_level, -1, -1):
         c_inp = b_inp - b ** 2
         c_level = integer_sqrt(c_inp)
-        print(c_inp, 'step b')
+        # print(c_inp, 'step b')
         if c_inp == 0:
             result.append(b)
             break
             # loop 2
 
-        for c in range(c_level, 0, -1):
+        for c in range(c_level, -1, -1):
             d_inp = c_inp - c ** 2
             d_level = integer_sqrt(d_inp)
-            print(d_inp, 'step d')
+            # print(d_inp, 'step d')
             if d_inp == 0:
                 result.append(c)
                 break
                 # loop 3
             for d in range(d_level, 0, -1):
                 e_inp = d_inp - d ** 2
-                print(e_inp, 'step e')
-                if e_inp == 0:
+                # print(e_inp, 'step e')
+                if e_inp == 0 and len(result) == 0:
                     result.append(d)
                     break
 
                 # end loop 3
             if len(result) != 0:
                 result.append(c)
+                break
 
             # end loop 2
         if len(result) != 0:
@@ -64,4 +64,4 @@ for a in range(a_level, 0, -1):
         result.append(a)
         break
 
-print(*result, "output")
+print(*result)
