38ab6fb81a6d9618bd4e1f1cd16f88420e2e8f4c
Kirill Setdekov <ksetdekov@gmail.com>
2020-10-10 12:56:04 +0300

    added 4 steps

--- lect 5 materials/w_cubes.py
+++ lect 5 materials/w_cubes.py
@@ -12,7 +12,59 @@ def c_root(k):
         x = (x - (x ** 3 - k) / (3 * x ** 2))//1
         if x ** 3 <= k < (x + 1) ** 3:
             break
-    return x
+    return int(x)
 
 
-print(c_root(n_inp))
+a_level = c_root(n_inp)
+for a in range(a_level, -1, -1):
+    b_inp = n_inp - a ** 2
+    b_level = c_root(b_inp)
+    # print(a, 'step a')
+    if b_inp == 0:
+        result.append(a)
+        break
+    # loop 1
+
+    for b in range(b_level, -1, -1):
+        c_inp = b_inp - b ** 2
+        c_level = c_root(c_inp)
+        # print(c_inp, 'step b')
+        if c_inp == 0:
+            result.append(b)
+            break
+            # loop 2
+
+        for c in range(c_level, -1, -1):
+            d_inp = c_inp - c ** 2
+            d_level = c_root(d_inp)
+            # print(d_inp, 'step d')
+            if d_inp == 0:
+                result.append(c)
+                break
+                # loop 3
+            for d in range(d_level, 0, -1):
+                e_inp = d_inp - d ** 2
+                # print(e_inp, 'step e')
+                if e_inp == 0 and len(result) == 0:
+                    result.append(d)
+                    break
+
+                # end loop 3
+            if len(result) != 0:
+                result.append(c)
+                break
+
+            # end loop 2
+        if len(result) != 0:
+            result.append(b)
+            break
+
+    # end loop 1
+    if len(result) != 0:
+        result.append(a)
+        break
+
+if len(result) == 0:
+    print('0')
+else:
+    print(*result)
