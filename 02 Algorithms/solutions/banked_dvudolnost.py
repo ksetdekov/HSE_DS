dc2db834f995d8f1bbba06e041479e4573695c65
ksetdekov <ksetdekov@gmail.com>
2020-11-21 19:24:39 +0300

    fixed banked_dvudolnost.py

--- 02 Algorithms/solutions/banked_dvudolnost.py
+++ 02 Algorithms/solutions/banked_dvudolnost.py
@@ -1,24 +1,25 @@
 import sys
 from collections import defaultdict
 
-n, m = list(map(int, input()))
+n, m = list(map(int, input().split()))
 graph = defaultdict(list)
 for i in range(m):
     u, v = map(int, input().split())
-    graph[u].append(v)
     graph[v].append(u)
+    graph[u].append(v)
 
 colour = [0 for i in range(n)]
 
 
-def dfs(vertex, c):
-    colour[vertex - 1] = True
+def dfs(v_inner, c):
+    colour[v_inner - 1] = c
 
-    for col in graph[vertex]:
-        if colour[u - 1] == 0:
-            dfs(u - 1, (c % 2) + 1)
-        elif colour[col] == c:
-            print("NO")
+    for u_inner in graph[v_inner]:
+        if colour[u_inner - 1] == 0:
+            dfs(u_inner, (c % 2) + 1)
+
+        elif colour[u_inner - 1] == c:
+            print('NO')
             sys.exit(0)
 
 
@@ -26,10 +27,10 @@ ans = []
 for i in range(n):
     v = i + 1
     if colour[i] == 0:
-        dfs(i + 1, 1)
+        dfs(v, 1)
 
     if colour[i] == 1:
         ans.append(v)
 
-print("YES")
+print('YES')
 print(*ans)
