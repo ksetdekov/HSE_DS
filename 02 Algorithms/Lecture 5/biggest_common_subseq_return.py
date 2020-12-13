eecd413a8039b603aee406ea3680425813e7a59a
ksetdekov <ksetdekov@gmail.com>
2020-12-13 21:27:47 +0300

    biggest_common_subseq_return.py

--- 02 Algorithms/Lecture 5/biggest_common_subseq_return.py
+++ 02 Algorithms/Lecture 5/biggest_common_subseq_return.py
@@ -3,6 +3,11 @@ p1 = list(map(int, input().split()))
 n2 = int(input())
 p2 = list(map(int, input().split()))
 
+# n1 = 3
+# p1 = [1, 2, 3]
+# n2 = 3
+# p2 = [ 2, 3, 1]
+
 
 # 1) если не равны значения максимум
 #
@@ -13,8 +18,9 @@ p2 = list(map(int, input().split()))
 # * значение сверху
 # * значение слева
 # * сверху слева +1
-def big_com_seq(a, b, la, lb):
-    ans = []
+def big_com_seq(a, b):
+    la = len(a)
+    lb = len(b)
     matrix = [[0 for _ in range(lb + 1)] for _ in range(la + 1)]
     matrix_ans = [[[] for _ in range(lb + 1)] for _ in range(la + 1)]
     for i in range(la):
@@ -23,23 +29,22 @@ def big_com_seq(a, b, la, lb):
             # print(matrix)
             matrix[i][j] = max(matrix[i - 1][j], matrix[i][j - 1])
             if matrix[i - 1][j] >= matrix[i][j - 1]:
-                matrix_ans[i][j] = matrix_ans[i - 1][j]
+                matrix_ans[i][j].extend(matrix_ans[i - 1][j])
             else:
-                matrix_ans[i][j] = matrix_ans[i][j - 1]
+                matrix_ans[i][j].extend(matrix_ans[i][j - 1])
             if a[i] == b[j]:
                 if matrix[i][j] > 1 + matrix[i - 1][j - 1]:
                     matrix_ans[i][j] = matrix_ans[i][j]
                 else:
-                    matrix_ans[i][j] = [matrix[i - 1][j - 1], b[i]]
+                    matrix_ans[i][j] = matrix_ans[i - 1][j - 1]
+                    matrix_ans[i][j].extend([a[i]])
                 matrix[i][j] = max(matrix[i][j], 1 + matrix[i - 1][j - 1])
 
-    print(ans)
-    print(matrix)
-    print(matrix_ans[la - 1][lb - 1])
-    return matrix[la - 1][lb - 1]
+    # print(ans)
+    # print(matrix)
+    # print(matrix_ans)
+    ans = " ".join(str(x) for x in matrix_ans[la - 1][lb - 1])
+    return ans
 
 
-print(big_com_seq(p1, p2, n1, n2))
-
-
-# на каждом шаге - найти какое у нас источник - потом пройтись по ним назад
+print(big_com_seq(p1, p2))
