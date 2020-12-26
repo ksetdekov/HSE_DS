8f0bdd07a22c5ee4a487da3e35ba3d0d444d1995
Kirill Setdekov <ksetdekov@gmail.com>
2020-12-26 17:02:30 +0300

    updated djekstra.py

---
+++ 02 Algorithms/Lecture 8/djekstra.py
@@ -0,0 +1,36 @@
+from heapq import heappop, heappush
+
+
+def dijkstra(start):
+    heap = []
+    dist = [float('inf') for _ in range(n)]
+    used = [False for _ in range(n)]
+
+    heappush(heap, (0, start))
+    dist[start] = 0
+
+    while len(heap) > 0:
+        d, v = heappop(heap)
+        if dist[v] < d or used[v]:
+            continue
+
+        used[v] = True
+
+        for u, c in graph[v]:
+            if d + c < dist[u]:
+                dist[u] = d + c
+                heappush(heap, (dist[u], u))
+
+    return dist
+
+
+if __name__ == "__main__":
+    n, m = map(int, input().split())
+    graph = [[] for j in range(n)]
+
+    for i in range(m):
+        v, u, c = map(int, input().split())
+        graph[v].append((u, c))
+        graph[u].append((v, c))
+
+    print(dijkstra(0))
