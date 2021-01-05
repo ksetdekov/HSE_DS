8ffae9657f6eca1b2ba2a04519cb4484a1ed070e
Kirill Setdekov <ksetdekov@gmail.com>
2021-01-06 02:14:19 +0300

    calculator solved

--- 02 Algorithms/winter_hw/654_e_calculator.py
+++ 02 Algorithms/winter_hw/654_e_calculator.py
@@ -4,24 +4,15 @@ N = int(input())
 def calc(n):
     steps = [0 for _ in range(n)]
     steps[0] = 0
-    # print(steps)
     for i in range(1, n):
         value = i + 1
-        # print(value, 'value')
-
+        variant = steps[i - 1] + 1
+        a, b, c = (variant, variant, variant)
         if value % 3 == 0:
-            # print('div 3')
-            if value % 2 == 0:
-                steps[i] = min(steps[(value // 3 - 1)]+1, steps[(value // 2 - 1)]+1)
-            else:
-                steps[i] = steps[(value // 3 - 1)] + 1
-        elif value % 2 == 0:
-            # print('div 2')
-
-            steps[i] = steps[(value // 2 - 1)] + 1
-        else:
-            steps[i] = steps[i - 1] + 1
-    # print(steps)
+            a = steps[(value // 3 - 1)] + 1
+        if value % 2 == 0:
+            b = steps[(value // 2 - 1)] + 1
+        steps[i] = min(a, b, c)
 
     return steps[-1]
 
