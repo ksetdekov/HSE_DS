e617e3542ba933dc0cf78164e41ea33198255187
Kirill Setdekov <ksetdekov@gmail.com>
2021-01-08 17:25:18 +0300

    this gives a solution different in style from the question

--- 02 Algorithms/winter_hw/graphs_259/259_d_izdevatelstvo.py
+++ 02 Algorithms/winter_hw/graphs_259/259_d_izdevatelstvo.py
@@ -143,25 +143,31 @@ class Graph:
 
 # Driver Code
 if __name__ == "__main__":
-    V = 9
+    V = int(input())
 
     g = Graph(V)
+    matrix = [list(map(int, input().split())) for _ in range(V)]
+    for i in range(len(matrix)):
+        for j in range(len(matrix[i])):
+            weight = matrix[i][j]
+            if weight > 0:
+                g.add_edge(i, j, weight)
 
     # Making above shown graph
-    g.add_edge(0, 1, 4)
-    g.add_edge(0, 7, 8)
-    g.add_edge(1, 2, 8)
-    g.add_edge(1, 7, 11)
-    g.add_edge(2, 3, 7)
-    g.add_edge(2, 8, 2)
-    g.add_edge(2, 5, 4)
-    g.add_edge(3, 4, 9)
-    g.add_edge(3, 5, 14)
-    g.add_edge(4, 5, 10)
-    g.add_edge(5, 6, 2)
-    g.add_edge(6, 7, 1)
-    g.add_edge(6, 8, 6)
-    g.add_edge(7, 8, 7)
+    # g.add_edge(0, 1, 4)
+    # g.add_edge(0, 7, 8)
+    # g.add_edge(1, 2, 8)
+    # g.add_edge(1, 7, 11)
+    # g.add_edge(2, 3, 7)
+    # g.add_edge(2, 8, 2)
+    # g.add_edge(2, 5, 4)
+    # g.add_edge(3, 4, 9)
+    # g.add_edge(3, 5, 14)
+    # g.add_edge(4, 5, 10)
+    # g.add_edge(5, 6, 2)
+    # g.add_edge(6, 7, 1)
+    # g.add_edge(6, 8, 6)
+    # g.add_edge(7, 8, 7)
 
     print(g.find_minimum_cycle())
 
