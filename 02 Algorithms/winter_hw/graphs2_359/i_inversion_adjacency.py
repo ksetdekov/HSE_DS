f10741f162f981b72469efd5b130c17862de37c7
Kirill Setdekov <ksetdekov@gmail.com>
2021-01-09 19:45:32 +0300

    works but this is memory intensive

--- 02 Algorithms/winter_hw/graphs2_359/i_inversion_adjacency.py
+++ 02 Algorithms/winter_hw/graphs2_359/i_inversion_adjacency.py
@@ -3,14 +3,16 @@ matrix = [[0 for _ in range(V)] for _ in range(V)]
 adjacency_list = []
 for _ in range(V):
     adjacency_list.append(list(map(int, input().split())))
-
+# print(adjacency_list)
 for vertexes in range(V):
     j = vertexes
     for elem in adjacency_list[vertexes]:
         i = elem - 1
         matrix[i][j] = 1
 
-print(matrix)
+# for r in matrix:
+#     print(*r)
+print(V)
 for r in matrix:
     neighbours = []
     for c in range(V):
