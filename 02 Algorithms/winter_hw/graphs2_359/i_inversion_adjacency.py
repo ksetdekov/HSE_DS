e59316b1b16a924d49673662746290b477299b83
Kirill Setdekov <ksetdekov@gmail.com>
2021-01-09 20:11:47 +0300

    solved) i_inversion_adjacency.py

--- 02 Algorithms/winter_hw/graphs2_359/i_inversion_adjacency.py
+++ 02 Algorithms/winter_hw/graphs2_359/i_inversion_adjacency.py
@@ -1,17 +1,14 @@
 V = int(input())
-matrix = [[0 for _ in range(V)] for _ in range(V)]
-adjacency_list = []
-for _ in range(V):
-    adjacency_list.append(list(map(int, input().split())))
-# print(adjacency_list)
+
 inverted_adj_list = [[] for _ in range(V)]
 for current in range(V):
     j = current
-    for elem in adjacency_list[current]:
+    read = list(map(int, input().split()))
+    for elem in read:
         inverted_adj_list[elem - 1].append(j + 1)
 
 # for r in matrix:
 #     print(*r)
 print(V)
 for row in inverted_adj_list:
-    print(*row)
+    print(" ".join(map(str, row)))
