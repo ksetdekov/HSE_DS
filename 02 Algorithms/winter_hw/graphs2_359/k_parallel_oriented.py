0242e1254bb84a5baf640b4f25fe52ce48ecb734
Kirill Setdekov <ksetdekov@gmail.com>
2021-01-10 21:38:11 +0300

    solved k_parallel_edges.py

--- 02 Algorithms/winter_hw/graphs2_359/k_parallel_oriented.py
+++ 02 Algorithms/winter_hw/graphs2_359/k_parallel_oriented.py
@@ -4,9 +4,6 @@ e_list = []
 result = "NO"
 for _ in range(E):
     current = list(map(int, input().split()))
-    # print(current)
-    inverted = current[::-1]
-    # print(inverted)
     if current in e_list:
         result = "YES"
     e_list.append(current)
